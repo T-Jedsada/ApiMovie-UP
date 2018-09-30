89585f25408a03791956eb714c4e4187f0ed119a
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-09-30 21:42:42 +0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const hapi = require('hapi')
-
+const Hapi = require('hapi');
 const mockMovie = require('./mock_movie')
 
-const server = new hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+var atatus = require("atatus-node");
+
+const server = Hapi.server({
+    host: 'localhost',
+    port: 3000
+});
 
 server.route({
     method: 'GET',
     path: '/movie',
-    handler: function (request, reply) {
-        reply(mockMovie);
+    handler: (_, h) => {
+        return h.response(mockMovie);
     }
 });
 
-server.start((err) => {
-    if (err) {
-        throw err;
+server.route({
+    method: 'GET',
+    path: '/error',
+    handler: (_, h) => {
+        return h.response("Hello World").code(500);
     }
-    console.log(`Server running at: ${server.info.uri}`);
 });
+
+// Start the server
+async function start() {
+    try {
+        await server.register({
+            name: "Test",
+            plugin: require('hapijs-status-monitor'),
+            options: {
+                title: 'My Status Monitor',
+                routeConfig: {
+                    auth: false
+                }
+            }
+        });
+        await server.start();
+        atatus.start({ apiKey: "fac9ab61b1a641a588608cd2d07550c9" });
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
